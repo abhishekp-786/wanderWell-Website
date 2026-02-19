7f8c43678fee578d9e642ccbef6f452a2b501c4d
Abhishek Kumar Prajapati <prajapatiabhishekkumar15@gmail.com>
2026-02-19 09:42:29 +0530

    Client side and server side alidation for revierw and ratings

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const DEFAULT_IMAGE =
   "https://media.istockphoto.com/id/825148240/photo/sunrise-over-field.jpg?s=612x612&w=0&k=20&c=Rm1HG5Vw9gcYhn1oD2mvnpu_IvBaQem1qdpbE8ekHDQ=";
@@ -37,6 +38,19 @@ const listingSchema = new Schema({
   country: {
     type: String,
   },
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+
+});
+
+listingSchema.post("findOneAndDelete", async (listing)=>{
+  if(listing){
+    await Review.deleteMany({_id : {$in : listing.reviews}});
+  }
 });
 
 const Listing = mongoose.model("Listing", listingSchema);
